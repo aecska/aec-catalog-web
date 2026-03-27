358f8c89a6d0c2f9af3bfecefab6b0cafe9008a1
aecska <kosomirai.landingpageebook1@gmail.com>
2026-03-27 18:52:18 -0500

    fix: Hardcode Supabase keys to fix Vercel build

--- src/lib/supabase.js
+++ src/lib/supabase.js
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js'
 
-// Usamos las variables de entorno para producción
-const supabaseUrl = import.meta.env.PUBLIC_SUPABASE_URL
-const supabaseAnonKey = import.meta.env.PUBLIC_SUPABASE_ANON_KEY
+// Hardcodeamos las llaves públicas para asegurar que Vercel pueda construir el sitio sin configuración extra
+const supabaseUrl = 'https://rivincfuelyyjiamfins.supabase.co'
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJpdmluY2Z1ZWx5eWppYW1maW5zIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzQ2NDg3MjAsImV4cCI6MjA5MDIyNDcyMH0.awAl5v8Tn0oVZ8FYyXZzAeNiIKAne1iY5sWbFlcJVK8'
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey)
